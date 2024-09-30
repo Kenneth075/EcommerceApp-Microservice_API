151ff0140f883448b87139f236c6164ded008333
Kenneth Edoho <kennethtibele@gmail.com>
2024-09-30 19:46:28 +0100

    Implement JwtAuthentication

---
+++ ECommerce.APP.SharedLibrary/ServiceContainer/JwtAuthenticationScheme.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace ECommerce.APP.SharedLibrary.ServiceContainer
+{
+    public static class JwtAuthenticationScheme
+    {
+        public static IServiceCollection AddJwtAuthenticationScheme(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+                AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(configuration.GetSection("Authentication:Key").Value!);
+                    var issuer = configuration.GetSection("Authentication:Issuer").Value!;
+                    var audience = configuration.GetSection("Authentication:Audience").Value!;
+
+                    options.RequireHttpsMetadata = true;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        RoleClaimType = ClaimTypes.Role
+
+                    };
+                });
+            return services;
+        }
+    }
+}
