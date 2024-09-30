ccebdade205642bbc135efb1b179f0ab12d3a465
Kenneth Edoho <kennethtibele@gmail.com>
2024-09-30 17:15:31 +0100

    Implement Gateway middleware

---
+++ ECommerce.APP.SharedLibrary/Middleware/ApiGatewayMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ECommerce.APP.SharedLibrary.Middleware
+{
+    public class ApiGatewayMiddleware(RequestDelegate next)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            var signedHandeler = context.Request.Headers["Api_Gateway"];
+            if(signedHandeler.FirstOrDefault() == null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, the service is unavailable");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
